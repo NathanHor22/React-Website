6c78cf0a9483bcbff8b5dbd4fa550b5013233331
Nathan Hor <nathanhor2001@gmail.com>
2025-12-02 12:24:46 +0000

    Navbar setup

---
+++ my-react-app/src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import syntechIcon from '../assets/SYNTHTECH_ICON.png'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul className="nav-links">
+        <li className="logo"><img src={syntechIcon} alt="Syntech Logo" /></li>
+        <li><a href="#about">Our Story</a></li>
+        <li><a href="#services">SynthMed</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+export default Navbar;
